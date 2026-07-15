a9b47b7dc4005cfc911b88b8e6a7d51d35173d57
HanseeVee <hansarthurvath@gmail.com>
2026-07-15 16:43:20 +0200

    minor fixes

--- run_and_judge_proposal.py
+++ run_and_judge_proposal.py
@@ -70,20 +70,20 @@ def prepare_dataset(dataset_name: str, sample_size: int = 210):
 
     # === UNBALANCED DATASETS ===
     elif dataset_name == "jailbreakbench-benign":
-        dataset = load_dataset("JailbreakBench/JBB-Behaviors", split="benign")
+        dataset = load_dataset("JailbreakBench/JBB-Behaviors", "behaviors", split="benign")
         sample_size = _check_single_sample_size(sample_size, dataset)
         dataset = dataset.select(range(sample_size))
         dataset = dataset.map(lambda elm: {
-            "prompt": elm["goal"],
+            "prompt": elm["Goal"],
             "label": 0
         })
 
     elif dataset_name == "jailbreakbench-harmful":
-        dataset = load_dataset("JailbreakBench/JBB-Behaviors", split="harmful")
+        dataset = load_dataset("JailbreakBench/JBB-Behaviors", "behaviors", split="harmful")
         sample_size = _check_single_sample_size(sample_size, dataset)
         dataset = dataset.select(range(sample_size))
         dataset = dataset.map(lambda elm: {
-            "prompt": elm["goal"],
+            "prompt": elm["Goal"],
             "label": 1
         })
 
