4cafc513570dee6209907191e13fd62c2391bdd0
Ramneek A. <104074322+ramneekagn@users.noreply.github.com>
2026-07-18 11:23:22 +0200

    Update run_judge_reclassify_eval.py

--- run_judge_reclassify_eval.py
+++ run_judge_reclassify_eval.py
@@ -5,9 +5,6 @@ from reclassify_overall import reclassify, and_rule, or_rule, majority_vote
 from run_evaluation_reworked import log_eval
 from dotenv import load_dotenv
 import os
-os.environ["HF_HUB_OFFLINE"] = "1"
-os.environ["HF_DATASETS_OFFLINE"] = "1"
-
 def run_pipeline(dataset_name, config_path, run_name, base_output_dir="results", config_name="safe_llm_config_base", sample_size=100, batch_size=8):
     name = f"{config_name}_{run_name}"
     dataset_dir = Path(base_output_dir) / name / dataset_name
